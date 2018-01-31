9309b215d2cd20d01f56e7eae4c2f94268f375f7
C00196266 <c00196266@itcarlow.ie>
2018-01-31 16:43:18 +0000

    Tweaking input

--- Princess/src/InputHandler.cpp
+++ Princess/src/InputHandler.cpp
@@ -2,27 +2,35 @@
 
 void InputHandler::handleInput(SDL_Event &e, SDL_Point &pos) 
 {
+	//if (e.key.keysym.sym == SDLK_w) {
+	//	pos.y--;
+	//}
+	//if (e.key.keysym.sym == SDLK_a) {
+	//	pos.x--;
+	//}
+	//if (e.key.keysym.sym == SDLK_s) {
+	//	pos.y++;
+	//}
+	//if (e.key.keysym.sym == SDLK_d) {
+	//	pos.x++;
+	//}
+
 	switch (e.type) {
 	case SDL_KEYDOWN:
 		switch (e.key.keysym.sym) {
 		case SDLK_w:
 			pos.y--;
 			break;
-
+	
 		case SDLK_a:
 			pos.x--;
 			break;
-
+	
 		case SDLK_s:
 			pos.y++;
 			break;
-
+	
 		case SDLK_d:
-			pos.x++;
-			break;
-
-		case SDLK_d && SDLK_s:
-			pos.y++;
 			pos.x++;
 			break;
 		}
