c2522333e897a50a8e3ba25e6800ce19748234e5
Kevin Boylan <idakev2396@gmail.com>
2018-02-05 18:11:42 +0000

    ARC-98 #time 1h Huge improvement to performance (collision issue)

--- Princess/include/CollisionSystem.h
+++ Princess/include/CollisionSystem.h
@@ -10,6 +10,9 @@ class CollisionSystem : public System
 public:
 	CollisionSystem();
 	~CollisionSystem();
-
+	int x1 = -22222;
+	int y1 = -22222;
+	int w1 = -22222;
+	int h1 = -22222;
 	void Update();
 };
--- Princess/src/CollisionSystem.cpp
+++ Princess/src/CollisionSystem.cpp
@@ -12,100 +12,90 @@ CollisionSystem::~CollisionSystem()
 
 void CollisionSystem::Update()
 {
+
+	int pcKeyOne = -1;
+	int ccKeyOne = -1;
+	std::string projectileCheck = "Projectile";
+	std::string playerCheck = "Player";
+
+
+	std::vector<int> posIndex;
+	std::vector<int> spriteIndex;
 	for (int i = 0; i < m_entities.size(); i++)
 	{
-		if (m_entities.at(i)->ID() == "Player" || m_entities.at(i)->ID() == "Projectile")
+		if (m_entities.at(i)->ID() == "Projectile" || m_entities.at(i)->ID() != "Player")
 		{
-
-
-			int pcKeyOne = -1;
-			int ccKeyOne = -1;
-			int pcKeyTwo = -1;
-			int ccKeyTwo = -1;
-			//std::string projectileCheck = "Projectile";
-			//std::string playerCheck = "Player";
-
 			for (int j = 0; j < m_entities.at(i)->GetComponents()->size(); j++)
 			{
-				if (m_entities.at(i)->GetComponents()->at(j)->Type() == "PC")
+				if (m_entities.at(i)->GetComponents()->at(j)->Type() == "PC" && m_entities.at(i)->ID() == projectileCheck)
 				{
-					pcKeyOne = j;
+					posIndex.push_back(j);
 				}
-				if (m_entities.at(i)->GetComponents()->at(j)->Type() == "SC")
+				if (m_entities.at(i)->GetComponents()->at(j)->Type() == "SC" && m_entities.at(i)->ID() == projectileCheck)
 				{
-					ccKeyOne = j;
+					spriteIndex.push_back(j);
 				}
 			}
 
-			for (int k = 0; k < m_entities.size(); k++)
+		}
+	}
+
+	//std::cout << "x2 " << posIndex.size() << std::endl;
+	for (int i = 0; i < m_entities.size(); i++)
+	{
+
+		//	int x1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().x;
+		for (int j = 0; j < m_entities.at(i)->GetComponents()->size(); j++)
+		{
+
+			if (m_entities.at(i)->ID() == playerCheck && m_entities.at(i)->GetComponents()->at(j)->Type() == "PC")
 			{
-				for (int j = 0; j < m_entities.at(k)->GetComponents()->size(); j++)
+				if (static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(j))->getPosition().x > -22222)
 				{
-					if (m_entities.at(k)->GetComponents()->at(j)->Type() == "PC")
-					{
-						pcKeyTwo = j;
-					}
-					if (m_entities.at(k)->GetComponents()->at(j)->Type() == "SC")
-					{
-						ccKeyTwo = j;
-					}
+					x1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(j))->getPosition().x;
+				}
+				if (static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(j))->getPosition().y > -22222)
+				{
+					y1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(j))->getPosition().y;
 				}
 
-				int x1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().x;
-				int y1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().y;
+				/*std::cout << "x1 " << x1 << std::endl;*/
 
-				int x2 = static_cast<PositionComponent*>(m_entities.at(k)->GetComponents()->at(pcKeyTwo))->getPosition().x;
-				int y2 = static_cast<PositionComponent*>(m_entities.at(k)->GetComponents()->at(pcKeyTwo))->getPosition().y;
+			}
+			if (m_entities.at(i)->ID() == playerCheck && m_entities.at(i)->GetComponents()->at(j)->Type() == "SC")
+			{
+				if (static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(j))->GetRect().w > -22222)
+				{
+					w1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(j))->GetRect().w;
+				}
+				if (static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(j))->GetRect().h > -22222)
+				{
+					h1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(j))->GetRect().h;
+				}
+			}
+		}
+		for (int j = 0; j < m_entities.at(i)->GetComponents()->size(); j++)
+		{
+			for (int p = 0; p < posIndex.size(); p++)
+			{
+				int x2 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(posIndex[p]))->getPosition().x;
+				int y2 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(posIndex[p]))->getPosition().y;
 
 
-				int w1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyOne))->GetRect().w;
-				int h1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyOne))->GetRect().h;
+				int w2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(spriteIndex[p]))->GetRect().w;
+				int h2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(spriteIndex[p]))->GetRect().h;
 
-				int w2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyTwo))->GetRect().w;
-				int h2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyTwo))->GetRect().h;
-
-
-				// BASIC COLLISION DETECTION
 				if (x1 != x2 || y1 != y2)
 				{
 					if ((x1 >= x2) && (x1 <= (x2 + w2))
 						&& (y1 >= y2 && y1 <= (y2 + w2)))
 					{
-						//std::cout << "COLLISION!!!" << std::endl;
+						std::cout << "COLLISION!!!" << std::endl;
 					}
 				}
-
-
-				//if (x1 != x2 || y1 != y2)
-				//{
-				//	if (x1 < (x2))
-				//	{
-				//		if (x1 + w1 >= x2)
-				//		{
-				//			std::cout << "FROM THE RIGHT" << std::endl;
-				//			int temp = x1 - 2;
-				//			static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->setX(temp);
-				//		}
-				//	}
-				//	if (x1 > x2)
-				//	{
-				//		if (x1 <= x2 + w2)
-				//		{
-				//			std::cout << "FROM THE RIGHT" << std::endl;
-				//			int temp = x1 + 2;
-				//			static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->setX(temp);
-				//		}
-				//	}	
-				//	/*}*/
-				//}
-
-				//std::cout << "x1 " << x1 << std::endl;
-				//std::cout << "y1 " << y1 << std::endl;
-				//std::cout << "x2 " << x2 << std::endl;
-				//std::cout << "y2 " << y2 << std::endl;
-
 			}
 		}
 	}
 }
+
 
