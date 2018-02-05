757156cea7789c015cdb5704a81194ded22799e3
C00196266 <c00196266@itcarlow.ie>
2018-02-05 16:48:59 +0000

    Working on optimising projectiles

--- Princess/src/CollisionSystem.cpp
+++ Princess/src/CollisionSystem.cpp
@@ -14,66 +14,139 @@ void CollisionSystem::Update()
 {
 	for (int i = 0; i < m_entities.size(); i++)
 	{
-		if (m_entities.at(i)->ID() == "Player" || m_entities.at(i)->ID() == "Projectile")
+		int pcKey = -1;
+		int ccKey = -1;
+
+		for (int j = 0; j < m_entities.at(i)->GetComponents()->size(); j++)
 		{
-
-
-			int pcKeyOne = -1;
-			int ccKeyOne = -1;
-			int pcKeyTwo = -1;
-			int ccKeyTwo = -1;
-			//std::string projectileCheck = "Projectile";
-			//std::string playerCheck = "Player";
-
-			for (int j = 0; j < m_entities.at(i)->GetComponents()->size(); j++)
+			if (m_entities.at(i)->GetComponents()->at(j)->Type() == "collision")
 			{
-				if (m_entities.at(i)->GetComponents()->at(j)->Type() == "PC")
+				ccKey = j;
+			}
+			else if (m_entities.at(i)->GetComponents()->at(j)->Type() == "PC")
+			{
+				pcKey = j;
+			}
+		}
+
+		if (ccKey >= 0)
+		{
+			if (m_entities.at(i)->ID() == "Player")
+			{
+
+				for (int j = 0; j < m_entities.size(); j++)
 				{
-					pcKeyOne = j;
-				}
-				if (m_entities.at(i)->GetComponents()->at(j)->Type() == "SC")
-				{
-					ccKeyOne = j;
+					int ccOtherKey = -1;
+					int pcOtherKey = -1;
+
+					for (int k = 0; k < m_entities.at(j)->GetComponents()->size(); k++)
+					{
+						if (m_entities.at(j)->GetComponents()->at(k)->Type() == "collision")
+						{
+							ccOtherKey = k;
+						}
+						else if (m_entities.at(j)->GetComponents()->at(k)->Type() == "PC")
+						{
+							pcOtherKey = k;
+						}
+					}
+
+					if (ccOtherKey >= 0)
+					{
+						if (m_entities.at(j)->ID() == "Projectile")
+						{
+							int x1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKey))->getPosition().x;
+							int y1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKey))->getPosition().y;
+
+							int x2 = static_cast<PositionComponent*>(m_entities.at(j)->GetComponents()->at(pcOtherKey))->getPosition().x;
+							int y2 = static_cast<PositionComponent*>(m_entities.at(j)->GetComponents()->at(pcOtherKey))->getPosition().y;
+
+
+							int w1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKey))->GetRect().w;
+							int h1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKey))->GetRect().h;
+
+							int w2 = static_cast<SpriteComponent*>(m_entities.at(j)->GetComponents()->at(ccOtherKey))->GetRect().w;
+							int h2 = static_cast<SpriteComponent*>(m_entities.at(j)->GetComponents()->at(ccOtherKey))->GetRect().h;
+
+
+							// BASIC COLLISION DETECTION
+							if (x1 != x2 || y1 != y2)
+							{
+								if ((x1 >= x2) && (x1 <= (x2 + w2))
+									&& (y1 >= y2 && y1 <= (y2 + w2)))
+								{
+									//std::cout << "COLLISION!!!" << std::endl;
+								}
+							}
+						}
+					}
 				}
 			}
+		}
+	}
 
-			for (int k = 0; k < m_entities.size(); k++)
-			{
-				for (int j = 0; j < m_entities.at(k)->GetComponents()->size(); j++)
-				{
-					if (m_entities.at(k)->GetComponents()->at(j)->Type() == "PC")
-					{
-						pcKeyTwo = j;
-					}
-					if (m_entities.at(k)->GetComponents()->at(j)->Type() == "SC")
-					{
-						ccKeyTwo = j;
-					}
-				}
+	//if (m_entities.at(i)->ID() == "Player" || m_entities.at(i)->ID() == "Projectile")
+	//{
+	//
+	//
+	//	int pcKeyOne = -1;
+	//	int ccKeyOne = -1;
+	//	int pcKeyTwo = -1;
+	//	int ccKeyTwo = -1;
+	//	//std::string projectileCheck = "Projectile";
+	//	//std::string playerCheck = "Player";
+	//
+	//	for (int j = 0; j < m_entities.at(i)->GetComponents()->size(); j++)
+	//	{
+	//		if (m_entities.at(i)->GetComponents()->at(j)->Type() == "PC")
+	//		{
+	//			pcKeyOne = j;
+	//		}
+	//		if (m_entities.at(i)->GetComponents()->at(j)->Type() == "SC")
+	//		{
+	//			ccKeyOne = j;
+	//		}
+	//	}
+	//
+	//	for (int k = 0; k < m_entities.size(); k++)
+	//	{
+	//		for (int j = 0; j < m_entities.at(k)->GetComponents()->size(); j++)
+	//		{
+	//			if (m_entities.at(k)->GetComponents()->at(j)->Type() == "PC")
+	//			{
+	//				pcKeyTwo = j;
+	//			}
+	//			if (m_entities.at(k)->GetComponents()->at(j)->Type() == "SC")
+	//			{
+	//				ccKeyTwo = j;
+	//			}
+	//		}
+	//
+	//		int x1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().x;
+	//		int y1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().y;
+	//
+	//		int x2 = static_cast<PositionComponent*>(m_entities.at(k)->GetComponents()->at(pcKeyTwo))->getPosition().x;
+	//		int y2 = static_cast<PositionComponent*>(m_entities.at(k)->GetComponents()->at(pcKeyTwo))->getPosition().y;
+	//
+	//
+	//		int w1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyOne))->GetRect().w;
+	//		int h1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyOne))->GetRect().h;
+	//
+	//		int w2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyTwo))->GetRect().w;
+	//		int h2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyTwo))->GetRect().h;
+	//
+	//
+	//		// BASIC COLLISION DETECTION
+	//		if (x1 != x2 || y1 != y2)
+	//		{
+	//			if ((x1 >= x2) && (x1 <= (x2 + w2))
+	//				&& (y1 >= y2 && y1 <= (y2 + w2)))
+	//			{
+	//				//std::cout << "COLLISION!!!" << std::endl;
+	//			}
+	//		}
+	////////////////////////////////////////////////////////////////////////////////////
 
-				int x1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().x;
-				int y1 = static_cast<PositionComponent*>(m_entities.at(i)->GetComponents()->at(pcKeyOne))->getPosition().y;
-
-				int x2 = static_cast<PositionComponent*>(m_entities.at(k)->GetComponents()->at(pcKeyTwo))->getPosition().x;
-				int y2 = static_cast<PositionComponent*>(m_entities.at(k)->GetComponents()->at(pcKeyTwo))->getPosition().y;
-
-
-				int w1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyOne))->GetRect().w;
-				int h1 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyOne))->GetRect().h;
-
-				int w2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyTwo))->GetRect().w;
-				int h2 = static_cast<SpriteComponent*>(m_entities.at(i)->GetComponents()->at(ccKeyTwo))->GetRect().h;
-
-
-				// BASIC COLLISION DETECTION
-				if (x1 != x2 || y1 != y2)
-				{
-					if ((x1 >= x2) && (x1 <= (x2 + w2))
-						&& (y1 >= y2 && y1 <= (y2 + w2)))
-					{
-						//std::cout << "COLLISION!!!" << std::endl;
-					}
-				}
 
 
 				//if (x1 != x2 || y1 != y2)
@@ -104,8 +177,8 @@ void CollisionSystem::Update()
 				//std::cout << "x2 " << x2 << std::endl;
 				//std::cout << "y2 " << y2 << std::endl;
 
-			}
-		}
-	}
+	//		}
+	//	}
+	//}
 }
 
