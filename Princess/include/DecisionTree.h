ac4fe0dc7ddfdfb490c88053b473c84f2b9f57e3
C00148197 <C00148197@itcarlow.ie>
2018-02-16 09:39:17 +0000

    ARC-163 #time 2h Decision Tree merge

---
+++ Princess/include/DecisionTree.h
@@ -0,0 +1,55 @@
+//the decision tree class
+#include "TreeNodes.h"
+class DecisionTree
+{
+public:
+	//functions
+	void RemoveNode(TreeNodes* node);
+	void DisplayTree(TreeNodes* CurrentNode);
+	void Output();
+	void Query();
+	void QueryTree(TreeNodes* rootNode);
+	void AddNode1(TreeNodes* base, int ExistingNodeID, int NewNodeID);
+	void AddNode2(int ExistingNodeID, int NewNodeID);
+	void CreateRootNode(int NodeID);
+	bool MakeDecision(TreeNodes* node);
+
+	int getDecision();
+
+	bool SearchAddNode1(TreeNodes* CurrentNode, int ExistingNodeID, int NewNodeID);
+	bool SearchAddNode2(TreeNodes* CurrentNode, int ExistingNodeID, int NewNodeID);
+	bool SearchAddNodesOn(TreeNodes* CurrentNode, int ExistingNodeID, int NewNodeID); //this perfectly searches the tree in order  1 2 4 5 1 3 6 7!
+	TreeNodes* m_RootNode;
+	bool searchNodes(TreeNodes* currentNode, int ExistingNodeID, int NewNodeID);
+	DecisionTree();
+
+	DecisionTree(int tarHp, int selfHP, int tarStr, int selfStr, int dist);
+	int calculatePathNodes(TreeNodes* root);
+
+	TreeNodes* m_lastNode;
+
+		////PHealth = random(60);
+		////MHealth = random(60);
+		////PStrength = random(50);
+		////MStrength = random(50);
+		////DistanceFBase = random(75);
+		////DistanceFMonster = random(75);
+	 ~DecisionTree();
+
+
+private: 
+	int m_Size = 0;
+	int m_decision = 0;
+
+	int PHealth;
+	int MHealth;
+	int PStrength;
+	int MStrength;
+	int Distance;
+
+
+
+	int test1;
+	int test2;
+	int test3;
+};
