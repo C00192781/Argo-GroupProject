89c028ccacaebd553e40a807a3a384bcc7807db3
MatTheGreat <c00190038@itcarlow.ie>
2018-02-20 21:45:39 +0000

    Attributes Momento Added

--- Princess/include/AttributesComponent.h
+++ Princess/include/AttributesComponent.h
@@ -3,6 +3,7 @@
 
 #include <list>
 #include "Component.h"
+#include "AttributesMomento.h"
 #include <iostream>
 
 enum class ModiferTypes
@@ -20,12 +21,6 @@ struct Modifer
 	ModiferTypes m_type;
 	int m_amount;
 	float m_duration;
-	//Modifer() 
-	//{ 
-	//	m_type = ModiferTypes::DAMAGE;
-	//	m_amount = 0;
-	//	m_duration = 0;
-	//};
 
 	Modifer(ModiferTypes type = ModiferTypes::DAMAGE, int amount = 0, float duration = 0) :m_type(type), m_amount(amount), m_duration(duration) {};
 };
@@ -52,7 +47,11 @@ public:
 	};
 
 
-	AttributesComponent(int health, int maxHealth, int armour, int maxArmour, int movement, int maxMovement) :m_health(health), m_maxHealth(maxHealth), m_armour(armour), m_maxArmour(maxArmour), m_movementSpeed(movement), m_maxMovementSpeed(maxMovement) { m_type = "attribute"; };
+	AttributesComponent(int health, int maxHealth, int armour, int maxArmour, int movement, int maxMovement) :m_health(health), m_maxHealth(maxHealth), m_armour(armour), m_maxArmour(maxArmour), m_movementSpeed(movement), m_maxMovementSpeed(maxMovement) 
+	{ 
+		m_type = "attribute";
+		momento = new AttributesMomento(health, maxHealth, armour, maxArmour, movement, maxMovement);
+	};
 
 	~AttributesComponent() 
 	{
@@ -79,6 +78,21 @@ public:
 	void MaxMovementSpeed(int movementSpeed) { m_maxMovementSpeed = movementSpeed; };
 
 	std::list<Modifer> * Modifers() { return m_modifers; };
+	
+	void Save()
+	{
+		momento = new AttributesMomento(m_health, m_maxHealth, m_armour, m_maxArmour, m_movementSpeed, m_maxMovementSpeed);
+	}
+
+	void Revert()
+	{
+		m_health = momento->Health();
+		m_maxHealth = momento->MaxHealth();
+		m_armour = momento->Armour();
+		m_maxArmour = momento->MaxArmour();
+		m_movementSpeed = momento->MovementSpeed();
+		m_maxMovementSpeed = momento->MaxMovementSpeed();
+	}
 
 private:
 	int m_armour;
@@ -89,6 +103,8 @@ private:
 
 	int m_movementSpeed;
 	int m_maxMovementSpeed;
+
+	AttributesMomento * momento;
 
 	std::list<Modifer> * m_modifers;
 };
